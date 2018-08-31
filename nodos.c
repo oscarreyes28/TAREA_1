4ab650061ed45b3d9b89b24ccf6e5e882d0b1494
oscarreyes28 <oscarreyes2897@gmail.com>
2018-08-31 12:29:57 -0500

    Tercer commit, agregue funcion que busca elemento

--- nodos.c
+++ nodos.c
@@ -7,10 +7,23 @@ struct Libro {
     int anioDePublicacion;
     struct Libro *next;
 };
+void buscar(struct Libro*, int);
 
 int main( int argc, char *argv[]){
 
-    struct Libro *lista;
-    // aqui va còdigo
-    return 0; dd
+    struct Libro *lista = NULL;
+    int aux;
+    buscar(lista, aux);
+    return 0;
+}
+
+void buscar(struct Libro *lista, int aux) // Función que buscará a un elemento de la lsita ligada, solo funcionará cuando ya tengamos una lista previamente hecah
+{
+    while (lista != NULL){
+        if (lista->anioDePublicacion == aux){
+        printf("Se encontro el elemento \n");
+        return;
+        }
+    lista = lista->next;}
+    printf("No se encontro el elemento!!");
 }
